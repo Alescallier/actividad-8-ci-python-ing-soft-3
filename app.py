20ee230b0aaa817f774bbc1534e33b4702e4d0ab
Alescallier <136367845+Alescallier@users.noreply.github.com>
2026-04-23 20:26:17 -0300

    Agregar funciones base en Python

---
+++ app.py
@@ -0,0 +1,18 @@
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a - b
+
+
+def es_par(numero):
+    return numero % 2 == 0
+
+
+def mayor_de_edad(edad):
+    return edad >= 18
+
+
+def saludar(nombre):
+    return f"Hola, {nombre}"
